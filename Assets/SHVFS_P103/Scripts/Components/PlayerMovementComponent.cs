f030874e4c6f2e9583ddf1114a299cc2e4f583fc
muggle2333 <1455380922@qq.com>
2023-02-21 20:58:25 +0800

    0215HW_Animation&Collider

--- Assets/SHVFS_P103/Scripts/Components/PlayerMovementComponent.cs
+++ Assets/SHVFS_P103/Scripts/Components/PlayerMovementComponent.cs
@@ -36,15 +36,16 @@ public class PlayerMovementComponent : MonoBehaviour
 
             movementDirection = new Vector3(inputDirection.x, 0f, inputDirection.y).normalized;
 
-            Vector3 hitNormal = IsBlocked(movementDirection);
+            //Shperecast the blocking structure
+            Vector3 hitNormal = GetBlockedCirection(movementDirection);
             float hitRelation = Vector3.Dot(hitNormal, movementDirection);
-            if(hitRelation>=0) //not block angle <=90
+            if(hitRelation>=0) //not block, angle <=90
             {
                 targetPosition = transform.position + movementSpeed * movementDirection * Time.deltaTime;
                 targetLookRotation = Vector3.Slerp(transform.forward, movementDirection, fracComplete);
                 transform.SetPositionAndRotation(targetPosition, Quaternion.LookRotation(targetLookRotation, Vector3.up));
             }
-            else //block angle >90
+            else //block, angle >90
             {
                 //if the direction is not totally opposite => move to the side
                 Vector3 hitTangent = new Vector3(hitNormal.z, 0, -1f*hitNormal.x);
@@ -71,14 +72,17 @@ public class PlayerMovementComponent : MonoBehaviour
     }
    
     //Raycast the collider
-    private Vector3 IsBlocked(Vector3 movementDirction)
+    private Vector3 GetBlockedCirection(Vector3 movementDirction)
     {
         //!!!Physics.SphereCast => cast shpere
         RaycastHit hitInfo;
         if (Physics.SphereCast(transform.position, raycastLength, movementDirction, out hitInfo, raycastLength))
         {
-            //Debug.Log(hitInfo.collider);
-            rayDistance = hitInfo.distance;
+            if(hitInfo.collider.GetComponent<StructureColliderComponent>() != null)
+            {
+                rayDistance = hitInfo.distance;
+                return hitInfo.normal;
+            }
         }
         //overlapShpere return collider[] not hit info
         /*Collider[] hitColliders = Physics.OverlapSphere(transform.position, raycastLength);
@@ -89,7 +93,7 @@ public class PlayerMovementComponent : MonoBehaviour
                 Debug.Log(hitCollider.name);
             }
         }*/
-        return hitInfo.normal;
+        return new Vector3(0,0,0);
     }
     private void OnDrawGizmos()
     {
